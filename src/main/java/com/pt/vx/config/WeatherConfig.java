a30b520de4887b7492d63080620020e1a5684237
xiaoluyehenbang <110893718+xiaoluyehenbang@users.noreply.github.com>
2023-07-26 10:00:00 +0800

    Update WeatherConfig.java

--- src/main/java/com/pt/vx/config/WeatherConfig.java
+++ src/main/java/com/pt/vx/config/WeatherConfig.java
@@ -9,12 +9,12 @@ public class WeatherConfig {
      * 天气相关的总开关
      * true则为开启，false则为关闭
      */
-    public static boolean OPEN = false;
+    public static boolean OPEN = true;
 
     /**
      * 天气资源的key
      */
-    public static final String weatherSourceKey = "your key";
+    public static final String weatherSourceKey = "6dae37053154458884df88cda2cc5a49";
 
     /**
      * 天气资源类型
