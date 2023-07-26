ee726ea11a6bd204f7b06b00e13948a11475b47c
xiaoluyehenbang <110893718+xiaoluyehenbang@users.noreply.github.com>
2023-07-26 10:46:30 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -39,7 +39,7 @@ public class WechatConfig {
     static {
         userList.add(getUser(
                 "ocZdZ63xyOnGt4ZsM6YOGZCnsdkU", //扫码关注你的测试号以后，测试平台会出现TA的微信号
-                "cAgRfQO7k4PMYApYJajJahQkl6pjlMin1a7X4K5LNhE", //要给这个人发送的模板ID
+                "ERUc9i0f-llZsN2IYZsPVPQMhd1R7Xg8i3iy5xIdn1E", //要给这个人发送的模板ID
                 "小胡", //咋称呼这个人
                 "石家庄市裕华区", //这个人的详细地址
                 "石家庄", //这个人在的城市
